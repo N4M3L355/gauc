1a84e7a015e2ac7060486cf80f096fc6ed88ead9
Adam <adamkral7560@gmail.com>
2019-03-11 16:55:58 +0100

    queue

--- helpers.js
+++ helpers.js
@@ -4,16 +4,19 @@ function htmlToElement(html) {
     template.innerHTML = html;
     return template.content.firstChild;
 }
+
+const int16checker = value => value > 0 && value < 2 ** 16 && value % 1 === 0;
+
 class Register {
 
     constructor(number, value = undefined) {
         this._number = number;
         this._value = value;
-        this.checkValue = value => value>0&&value<2**16&&value%1===0;
+        this.checkValue = int16checker;
         this._element = htmlToElement(`
-            <a class="collection-item register">${this._number}. register: 
-                <span id="register${this._number}" class="new badge" data-badge-caption="">${this._value}</span>
-            </a>
+            <div class="collection-item register">${this._number}. register: 
+                <span id="register${this._number}" class="registerValue new badge teal lighten-5 teal-text" data-badge-caption="">${this._value}</span>
+            </div>
         `);
         document.getElementById('registers').appendChild(this._element);
     }
@@ -23,7 +26,7 @@ class Register {
     }
 
     set value(value) {
-        if(this.checkValue(value)){
+        if (this.checkValue(value)) {
             document.getElementById(`register${this._number}`).innerHTML = value;
             this._value = value;
         }
@@ -39,36 +42,73 @@ class DebugState {
     }
 
 }
-show = (element,show) => show?element.classList.add("hidden"):element.classList.remove("hidden");
+
+class Queue {
+    constructor() {
+        this._queue = [];
+        this._offset = 0;
+        this.checkValue = int16checker;
+        this.enqueue = item => {
+            if (this.checkValue(item)) {
+                item = this._queue.push(item);
+                document.getElementById('queue').innerHTML = this._queue.slice(this._offset).join(' ');
+                return item;
+            }
+        };
+        this.dequeue = () => {
+            if (this._queue.length === 0) return undefined;
+            let item = this._queue[this._offset];
+            if (++this._offset * 2 >= this._queue.length) {
+                this._queue = this._queue.slice(this._offset);
+                this._offset = 0;
+            }
+            document.getElementById('queue').innerHTML = this._queue.slice(this._offset).join(' ');
+
+            return item;
+
+        };
+        this.peek = () => (this._queue.length > 0 ? this._queue[this._offset] : undefined);
+
+    }
+
+    get length() {
+        return this._queue.length - this._offset;
+    }
+
+
+}
+
+show = (element, show) => show ? element.classList.add("hidden") : element.classList.remove("hidden");
 const numberOfRegisters = 15;
 let registers, queue, debugState;
 let startDebug = () => debugStartHelper(document.getElementById('code').value);
 let stopDebug = () => debugStopHelper();
 
-debugStartHelper = (rawInput) => {
-    show(document.getElementById("stepButton"),false);
+debugStartHelper = (rawInput) => {
+    show(document.getElementById("stepButton"), false);
     document.getElementById("debugButton").innerHTML = "<i class=\"material-icons left\">stop</i>Stop Debugging";
-    document.getElementById("debugButton").addEventListener("click",stopDebug);
+    document.getElementById("debugButton").addEventListener("click", stopDebug);
     document.getElementById("debugButton").classList.add('red');
-    document.getElementById("debugButton").removeEventListener("click",startDebug);
+    document.getElementById("debugButton").removeEventListener("click", startDebug);
 
-     debugState = new DebugState(rawInput.split(String.fromCharCode(10)).map(x => x.trim()),0);
+    debugState = new DebugState(rawInput.split(String.fromCharCode(10)).map(x => x.trim()), 0);
 };
 
 debugStopHelper = () => {
-    show(document.getElementById("stepButton"),true);
+    show(document.getElementById("stepButton"), true);
     document.getElementById("debugButton").innerHTML = "<i class=\"material-icons left\">bug_report</i>Debug";
-    document.getElementById("debugButton").addEventListener("click",startDebug);
+    document.getElementById("debugButton").addEventListener("click", startDebug);
     document.getElementById("debugButton").classList.remove('red');
-    document.getElementById("debugButton").removeEventListener("click",stopDebug);
+    document.getElementById("debugButton").removeEventListener("click", stopDebug);
 };
 
 function postLoad() {
-    document.getElementById("stepButton").addEventListener("click", () => step(debugState));
-    document.getElementById("runButton").addEventListener("click",() => run(document.getElementById('code').value));
-    document.getElementById("runButton").addEventListener("click",() => debugStopHelper());
-    document.getElementById("debugButton").addEventListener("click",startDebug);
+    document.getElementById("stepButton").addEventListener("click", () => step(debugState));
+    document.getElementById("runButton").addEventListener("click", () => run(document.getElementById('code').value));
+    document.getElementById("runButton").addEventListener("click", () => debugStopHelper());
+    document.getElementById("debugButton").addEventListener("click", startDebug);
     registers = Array(numberOfRegisters).fill(0).map((x, i) => new Register(i));
+    queue = new Queue();
 }
 
 if (window.addEventListener) window.addEventListener("load", postLoad, false);
